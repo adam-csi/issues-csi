6ae16cf70d69884d4e2c2a3d45f36f4e32b7162b
Adam Daube <adam@clearscienceinc.com>
2016-06-07 09:25:22 -0400

    file selection works

--- app.js
+++ app.js
@@ -4,18 +4,64 @@
 
     var app = angular.module('app', []);
 
-    app.controller('MainController', function MainController() {
-      var vm = this;
+    app.directive('fileModel', ['$parse', function($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                var modelSetter = model.assign;
 
-      vm.onSubmit = onSubmit;
-      vm.model = {};
-      vm.options = {};
+                element.bind('change', function() {
+                    scope.$apply(function() {
+                        modelSetter(scope, element[0].files[0]);
+                    });
+                });
+            }
+        };
+    }]);
 
-      function onSubmit() {
-        if (vm.form.$valid) {
-          vm.options;
+    app.service('fileUpload', ['$http', function($http) {
+        this.uploadFileToUrl = function(file, uploadUrl) {
+            var fd = new FormData();
+            fd.append('file', file);
+            $http.post(uploadUrl, fd, {
+                    transformRequest: angular.identity,
+                    headers: {
+                        'Content-Type': undefined
+                    }
+                })
+                .success(function() {})
+                .error(function() {});
         }
-      }
+    }]);
 
-  });
+    app.controller('myCtrl', ['$scope', 'fileUpload', function($scope, fileUpload) {
+
+        $scope.uploadFile = function() {
+            var file = $scope.myFile;
+            console.log('file is ');
+            console.dir(file);
+            var uploadUrl = "/fileUpload";
+            fileUpload.uploadFileToUrl(file, uploadUrl);
+        };
+
+    }]);
+
+
+    app.controller('MainController', function MainController() {
+        var vm = this;
+
+        vm.onSubmit = onSubmit;
+        vm.model = {};
+        vm.options = {};
+
+        function onSubmit() {
+            if (vm.form.$valid) {
+                vm.options;
+            }
+        }
+
+    });
+
+
 })();
