4f467afa5b8c946ccb4ce8c392b1bd39b913a173
Adam Daube <adam@clearscienceinc.com>
2016-06-08 12:54:40 -0400

    version 1.0

--- submit.php
+++
@@ -1,9 +0,0 @@
-<body>
-<?php if (isset($success) && $success) { ?>
-<h4>Issue submitted!</h4>
-Thank you for contacting us. We will be in touch with you very soon.
-<?php } else { ?>
-<h1>Error ocurred</h1>
-Please contact Clear Science Inc. directly via email with your issue.
-<?php } ?>
-</body>
